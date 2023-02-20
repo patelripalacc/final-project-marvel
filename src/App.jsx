0d6ba1da4166f5a0e167a9a47e615b1e3f6e9d5b
ripalkumar <patelripalacc@gmail.com>
2023-02-20 11:03:37 -0500

    FINAL

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { Footer } from "./components/Footer";
 // Import dependencies
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+
 function App() {
   return (
     <BrowserRouter>
